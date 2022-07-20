e2ad85fc91d144d465b61e87319ceec91128e509
Baljinder Singh <yamenshahin@gmail.com>
2022-07-20 21:04:43 +0200

    update ver2

--- test_git.php
+++ test_git.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Test Git
  * Plugin URI:        #
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Author:            #
  * Author URI:        #
  * License:           GPL-2.0+
